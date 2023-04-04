d0053306b6a4d02ca6152af4e318f869b8342e64
Ankush Pandey <ankush.pandey@trilogy.com>
2023-04-04 14:43:32 +0000

    bucket Names updated

--- lib/cdk-project-stack2.ts
+++ lib/cdk-project-stack2.ts
@@ -6,7 +6,9 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 export class cdk_project_stack2 extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
-        const FristBucket = new s3.Bucket(this, "Bucket_1")
-        new s3.Bucket(this, "Bucket_2")
+        const s3FirstBucketName = Math.floor(Math.random() * 1000).toString().padStart(3, '0');
+        const s3SecondBucketName = Math.floor(Math.random() * 1000).toString().padStart(3, '0');
+        const FristBucket = new s3.Bucket(this, "Bucket_1"+"cloudfix-linter-demo"+s3FirstBucketName)
+        new s3.Bucket(this, "Bucket_2"+"cloudfix-linter-demo"+s3SecondBucketName)
     }
 }

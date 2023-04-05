27d7ad1dac54810e77c805e6e74e92223e5f9ccf
Ankush Pandey <ankush.pandey@trilogy.com>
2023-04-05 07:22:11 +0000

    check for env vars implemented

--- bin/cdk-project.ts
+++ bin/cdk-project.ts
@@ -5,18 +5,33 @@ import { cdkProjectStack } from '../lib/cdk-project-stack';
 import { cdkProjectSimpleResources } from '../lib/cdk-project-stack2'
 import { Tags } from 'aws-cdk-lib';
 const app = new cdk.App();
-const stack = new cdkProjectStack(app, 'CDKDemoStack', {
+function envVarsPresent() {
+  const accountId = process.env.ACCOUNT_ID
+  const region = process.env.REGION
+  if ((!region && !accountId) || process.env.ACCOUNT_ID === '' || process.env.REGION === '') {
+    return false
+  }
+  return true
+}
 
-  env: {
-    account: process.env.ACCOUNT_ID,
-    region: process.env.REGION
-  }
-});
-const stack2 = new cdkProjectSimpleResources(app, 'CDKDemoStack2', {
-  env: {
-    account: process.env.ACCOUNT_ID,
-    region: process.env.REGION
-  }
-});
-Tags.of(stack).add("Project", "cloudfix-linter-cdk");
-Tags.of(stack2).add("Project", "cloudfix-linter-cdk");
+if (!envVarsPresent()) {
+  console.log("ENV VARS ARE NOT SET. Please export ACCOUNT_ID and REGION to deploy stacks")
+
+} else {
+
+  const stack = new cdkProjectStack(app, 'CDKDemoStack', {
+
+    env: {
+      account: process.env.ACCOUNT_ID,
+      region: process.env.REGION
+    }
+  });
+  const stack2 = new cdkProjectSimpleResources(app, 'CDKDemoStack2', {
+    env: {
+      account: process.env.ACCOUNT_ID,
+      region: process.env.REGION
+    }
+  });
+  Tags.of(stack).add("Project", "cloudfix-linter-cdk");
+  Tags.of(stack2).add("Project", "cloudfix-linter-cdk");
+}

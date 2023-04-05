280aff5c210afafb09de4aa2236f3bfbbefb4b74
Ankush Pandey <ankush.pandey@trilogy.com>
2023-04-05 06:09:24 +0000

    deployed IDs can being used as constants

--- lib/cdk-project-stack.ts
+++ lib/cdk-project-stack.ts
@@ -7,37 +7,38 @@ import { aws_efs as efs, aws_s3 as s3 } from 'aws-cdk-lib';
 import { EbsDeviceVolumeType, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { BucketEncryption } from 'aws-cdk-lib/aws-s3';
 import * as cdk from 'aws-cdk-lib';
+import { AVAILABILITYZONES, PRIVATE_SUBNETIDS, PRIVATE_SUBNET_ROUTETABLEIDS, PUBLIC_SUBNETIDS, PUBLIC_SUBNET_ROUTETABLEIDS, VPC_ID } from './constants';
 
-export class cdkProjectStack extends Stack {
+export class CdkDemoStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
 
     //DynamoDB table 
-    const table = new dynamodb.Table(this, 'cloudfixlinter-cf', {
+    const table = new dynamodb.Table(this, 'cloudfix-cdk-demo-Dynamotable', {
       partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
     });
 
     //Ebs vol
-    const volume = new ec2.Volume(this, 'cloudfix-cf-DataVolume', {
+    const volume = new ec2.Volume(this, 'cloudfix-cdk-demo-DataVolume', {
       availabilityZone: 'us-east-1a',
       size: Size.gibibytes(10),
       volumeType: EbsDeviceVolumeType.GENERAL_PURPOSE_SSD,
     })
 
     // vpc 
-    const DefaultVpc = Vpc.fromVpcAttributes(this, 'cloudfix-cf-vpcdev', {
-      vpcId: 'vpc-02badd8abb988e06e',
-      availabilityZones: ['us-east-1a'],
-      privateSubnetIds: ['subnet-0a1186feed91f0165'],
-      publicSubnetIds: ['subnet-0c53c3bc75d9b66b2', 'subnet-0ad82a9a46e5aaf68'],
-      publicSubnetRouteTableIds: ['rtb-055316860c9497d01', 'rtb-08f90b2a360aeb299'],
-      privateSubnetRouteTableIds: ['rtb-08f90b2a360aeb299']
+    const DefaultVpc = Vpc.fromVpcAttributes(this, 'cloudfix-cdk-demo-Vpc', {
+      vpcId: VPC_ID,
+      availabilityZones: AVAILABILITYZONES,
+      privateSubnetIds: PRIVATE_SUBNETIDS,
+      publicSubnetIds: PUBLIC_SUBNETIDS,
+      publicSubnetRouteTableIds: PUBLIC_SUBNET_ROUTETABLEIDS,
+      privateSubnetRouteTableIds: PRIVATE_SUBNET_ROUTETABLEIDS
     });
 
     // EC2 instance - 1
-    const instance = new ec2.Instance(this, 'cloudfix-cf-AppServer', {
+    const instance = new ec2.Instance(this, 'cloudfix-cdk-demo-AppServer', {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
       machineImage: ec2.MachineImage.latestAmazonLinux({
         generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
@@ -46,7 +47,7 @@ export class cdkProjectStack extends Stack {
     });
 
     //Ec2 instance - 2
-    new ec2.Instance(this, `cloudfix-cf-instance-appserver`, {
+    new ec2.Instance(this, `cloudfix-cdk-demo-instance-appserver`, {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
       machineImage: new ec2.GenericLinuxImage({
         'us-east-1': 'ami-09d56f8956ab235b3',
@@ -56,14 +57,14 @@ export class cdkProjectStack extends Stack {
     });
 
     // NAT gateway
-    new ec2.CfnNatGateway(this, 'cloudfix-cf-Natgateway', {
-      subnetId: 'subnet-0ad82a9a46e5aaf68',
+    new ec2.CfnNatGateway(this, 'cloudfix-cdk-demo-Natgateway', {
+      subnetId: PRIVATE_SUBNETIDS[1],
       // the properties below are optional
       connectivityType: 'private',
     });
 
     // VPC endpoint
-    new ec2.GatewayVpcEndpoint(this, 'cloudfix-cf-MyVpcEndpoint', {
+    new ec2.GatewayVpcEndpoint(this, 'cloudfix-cdk-demo-MyVpcEndpoint', {
       service: { name: 'com.amazonaws.us-east-1.s3' },
       vpc: DefaultVpc,
       subnets: [
@@ -72,14 +73,15 @@ export class cdkProjectStack extends Stack {
 
     });
 
+    const bucketNamePrefix = Math.floor(Math.random() * 1000).toString().padStart(3, '0');
     //s3 Bucket - 1
-    const bucket = new s3.Bucket(this, 'cloudfix-cf-s3-1', {
+    const bucket = new s3.Bucket(this, 'cloudfix-cdk-demo-s3-1' + bucketNamePrefix, {
       bucketName: "cloudfix-s3-bucket-1",
       versioned: true,
       accessControl: s3.BucketAccessControl.PRIVATE,
     });
 
-    const bucket2 = new s3.Bucket(this, 'cloudfix-cf-s3-2', {
+    const bucket2 = new s3.Bucket(this, 'cloudfix-cdk-demo-s3-2' + bucketNamePrefix, {
       bucketName: "cloudfix-s3-bucket-2",
       encryption: BucketEncryption.S3_MANAGED,
       versioned: true,
@@ -87,30 +89,10 @@ export class cdkProjectStack extends Stack {
     });
 
     // EFS file system
-    new efs.FileSystem(this, 'cloudfix-cf-MyFileSystem', {
+    new efs.FileSystem(this, 'cloudfix-cdk-demo-MyFileSystem', {
       vpc: DefaultVpc,
       encrypted: true,
     });
-
-
-    //aws_s3_bucket_public_access_block
-
-    //aws_s3_bucket_versioningterraforte
-
-    //neptune DBCluster
-    // const cluster = new neptune.DatabaseCluster(this, 'cloufix-cf-MyCluster', {
-    //   instanceType: neptune.InstanceType.R5_LARGE,
-    //   vpc: DefaultVpc,
-    //   removalPolicy: cdk.RemovalPolicy.DESTROY,
-    //   port: 8182,
-    //   iamAuthentication: true,
-    //   backupRetention: cdk.Duration.days(7),
-    //   preferredBackupWindow: '03:00-04:00',
-    //   vpcSubnets:{
-    //     subnetType: SubnetType.PUBLIC 
-    //   }
-    // });
-
 
   }
 }
--- lib/cdk-project-stack2.ts
+++ lib/cdk-project-stack2.ts
@@ -3,10 +3,12 @@ import { Construct } from 'constructs';
 import { aws_s3 as s3 } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 
-export class cdk_project_stack2 extends Stack {
+export class CdkDemoStack2 extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
-        const FristBucket = new s3.Bucket(this, "Bucket_1")
-        new s3.Bucket(this, "Bucket_2")
+        const s3FirstBucketName = Math.floor(Math.random() * 1000).toString().padStart(3, '0');
+        const s3SecondBucketName = Math.floor(Math.random() * 1000).toString().padStart(3, '0');
+        const FristBucket = new s3.Bucket(this, "cloudfix-cdk-demo" + "-stack2-s3Bucket-1" + s3FirstBucketName)
+        new s3.Bucket(this, "cloudfix-cdk-demo" + "-stack2-s3Bucket-1" + s3SecondBucketName)
     }
 }

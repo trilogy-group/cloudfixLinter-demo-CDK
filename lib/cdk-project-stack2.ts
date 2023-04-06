e861f08d61187d60a182107e3c18a20431e8268b
Yogesh Kumar <yogesh.kumar@trilogy.com>
2023-04-07 03:12:25 +0530

    Remove bucket name randomisation

--- lib/cdk-project-stack2.ts
+++ lib/cdk-project-stack2.ts
@@ -2,12 +2,12 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { aws_s3 as s3 } from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { BRANCH_NAME } from './constants';
 
 export class CdkProjectSimpleResources extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
-        const s3FirstBucketName = Math.floor(Math.random() * 1000).toString().padStart(3, '0');
 
-        const FristBucket = new s3.Bucket(this, "cloudfix-cdk-demo" + "-stack2-s3Bucket-1" + s3FirstBucketName)
+        const FristBucket = new s3.Bucket(this, "cloudfix-cdk-demo" + "-stack2-s3Bucket-1" + BRANCH_NAME)
     }
 }
